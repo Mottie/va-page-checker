f9bef6c5d6f7d0995870146301fce9a57940967d
Rob Garrison <wowmotty@gmail.com>
2025-07-05 11:32:38 -0500

    Remove v1 web component selectors

--- scripts/build-version.js
+++ scripts/build-version.js
@@ -1,15 +1,12 @@
 /* global COLORS, add, webComponentSelectors, USES_BEFORE */
 /* exported buildVersionCss */
 
-// Build v1 and v3 web component CSS
+// Build web component CSS
 /* eslint-disable-next-line no-unused-vars */
 const buildVersionCss = () => [
 	...webComponentSelectors().split(',').flatMap(wc => {
 		const useAfter = USES_BEFORE.has(wc);
-		return [
-			add(`${wc}[uswds="false"]`, `${wc} v1`, COLORS.ERROR, useAfter),
-			add(`${wc}:not([uswds="false"])`, `${wc} v3`, COLORS.USWDS, useAfter),
-		].flat();
+		return add({ selector: wc, message: wc, color: COLORS.USWDS, useAfter });
 	}),
 
 	// Fix open va-modal
